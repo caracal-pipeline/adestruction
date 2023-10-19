3a581b882c7464200ebd58604686f9d0142048a7
sphemakh <sphemakh@gmail.com>
2023-10-19 11:55:59 +0200

    Fixes #2

--- caracal_destruct/main.py
+++ caracal_destruct/main.py
@@ -9,54 +9,26 @@ from caracal.workers.worker_administrator import WorkerAdministrator
 import logging
 import stimela
 from caracal.dispatch_crew import config_parser
-from .distribute import Scatter
 from .slurm.run import SlurmRun
 import ruamel.yaml as yaml
 from caracal.schema import SCHEMA_VERSION
 
 
-def get_obsconf(options, config):
+
+def init_pipeline(options, config):
     # setup piping infractructure to send messages to the parent
     workers_directory = os.path.join(caracal.PCKGDIR, "workers")
     backend = config['general']['backend']
     if options.container_tech and options.container_tech != 'default':
         backend = options.container_tech
 
-    def __run(debug=False):
-        """ Executes pipeline """
-#       with stream_director(log) as director:  # stdout and stderr needs to go to the log as well -- nah
-        try:
-            pipeline = WorkerAdministrator(config,
-                           workers_directory,
-                           add_all_first=False, prefix=options.general_prefix,
-                           configFileName=options.config, singularity_image_dir=options.singularity_image_dir,
-                           container_tech=backend, start_worker=options.start_worker, end_worker=options.end_worker)
-            pipeline.run_workers()
-        except SystemExit as e:
-            log.error(f"A pipeline worker initiated sys.exit({e.code}). This is likely a bug, please report.")
-            log.info(f"More information can be found in the logfile at {caracal.CARACAL_LOG}")
-            log.info(f"You are running version {caracal.__version__}", extra=dict(logfile_only=True))
-            if debug:
-                log.warning("you are running with -debug enabled, dropping you into pdb. Use Ctrl+D to exit.")
-                pdb.post_mortem(sys.exc_info()[2])
-            sys.exit(1) 
+    pipeline = WorkerAdministrator(config,
+               workers_directory,
+               add_all_first=False, prefix=options.general_prefix,
+               configFileName=options.config, singularity_image_dir=options.singularity_image_dir,
+               container_tech=backend, start_worker=options.start_worker, end_worker=options.end_worker)
+    return pipeline
 
-        except KeyboardInterrupt:
-            log.error("Ctrl+C received from user, shutting down. Goodbye!")
-        except Exception as exc:
-            log.error(f"{exc}, [{type(exc).__name__}]", extra=dict(boldface=True))
-            log.info(f"  More information can be found in the logfile at {caracal.CARACAL_LOG}")
-            log.info(f"  You are running version {caracal.__version__}", extra=dict(logfile_only=True))
-            for line in traceback.format_exc().splitlines():
-                log.error(line, extra=dict(traceback_report=True))
-            if debug:
-                log.warning("you are running with -debug enabled, dropping you into pdb. Use Ctrl+D to exit.")
-                pdb.post_mortem(sys.exc_info()[2])
-            log.info("exiting with error code 1")
-            sys.exit(1)  # indicate failure
-        return pipeline
-
-    return __run()
 
 @click.command()
 @click.argument("config_file", type=str)
@@ -113,13 +85,16 @@ def driver(config_file, nband, bands, batchconfig):
     # LOG config file to screen
     parser.log_options(config)
 
-    obsconf = get_obsconf(options=options, config=config)
-    
-    scatter = Scatter(obsconf)
-    scatter.set(bands or nband)
+    # Initialise main pipeline
+    pipeline = init_pipeline(options, config)
+
     with open(batchconfig, "r") as stdr:
         batchconfig = yaml.load(stdr, yaml.RoundTripLoader, version=(1, 1))
-    runit = SlurmRun(scatter=scatter, config=batchconfig)
 
+    runit = SlurmRun(pipeline, config=batchconfig)
+    # Run CARACal obsconf worker and exit. This is required to get the observation information 
+    # needed to distribute the work
+    runit.init_destruction()
+    # Submit distrbuted jobs to Slurm
     runit.submit()
                     
--- caracal_destruct/slurm/run.py
+++ caracal_destruct/slurm/run.py
@@ -1,17 +1,55 @@
 from simple_slurm import Slurm
-from caracal import log as LOG
+from caracal import log
+import caracal
+from caracal_destruct.distribute import Scatter
 import os
+import sys
 import re
+import traceback
 
 
 class SlurmRun():
-    def __init__(self, scatter, config):
-        self.scatter = scatter
-        self.pipeline = self.scatter.pipeline
+    def __init__(self, pipeline, config):
+        self.pipeline = pipeline
         self.config = config
         
-        #self.scatter.greet()
         self.slurm = Slurm(**self.config)
+        
+        self.command_line = ["caracal --general-backend singularity"]
+        self.command_line += [f"--general-rawdatadir {self.pipeline.rawdatadir}"]
+        self.command_line += [f"--config {self.pipeline.config_file}"]
+        
+    
+    def init_destruction(self):
+        command_line = self.command_line + ["--end-worker obsconf"]
+        obsconf = Slurm(**self.config)
+        log.info("Running CARACal obsconf worker to get observation information. ")
+        obsconf.srun(command_line)
+        log.info("CARACal obsconf files created. Ready to distribute")
+
+        self.run_obsconf()
+        self.scatter = Scatter(self.pipeline)
+
+    def run_obsconf(self):
+        try:
+            self.pipeline.run_workers()
+        except SystemExit as e:
+            log.error(f"The CARACal 'obsconf' initiated sys.exit({e.code}). This is likely a bug, please report.")
+            log.info(f"More information can be found in the logfile at {caracal.CARACAL_LOG}")
+            log.info(f"You are running version {caracal.__version__}", extra=dict(logfile_only=True))
+
+        except KeyboardInterrupt:
+            log.error("Ctrl+C received from user, shutting down. Goodbye!")
+        except Exception as exc:
+            log.error(f"{exc}, [{type(exc).__name__}]", extra=dict(boldface=True))
+            log.info(f"  More information can be found in the logfile at {caracal.CARACAL_LOG}")
+            log.info(f"  You are running version {caracal.__version__}", extra=dict(logfile_only=True))
+            for line in traceback.format_exc().splitlines():
+                log.error(line, extra=dict(traceback_report=True))
+            log.info("exiting with error code 1")
+            sys.exit(1)  # indicate failure
+        return self.pipeline
+
     
 
     def submit(self):
@@ -20,11 +58,11 @@ class SlurmRun():
         """
         
         pipeline = self.pipeline
-
+        if not hasattr(self, "scatter"):
+            raise RuntimeError("Slurm Run scatter has not been set.")
+                       
         # Build caracal command
-        command_line = ["caracal --general-backend singularity"]
-        command_line += [f"--general-rawdatadir {pipeline.rawdatadir}"]
-        command_line += [f"--config {pipeline.config_file}"]
+        command_line = list(self.command_line)
 
         self.var = "--transform-split_field-spw"
         self.values = self.scatter.bands
@@ -36,8 +74,8 @@ class SlurmRun():
             outdir = os.path.join(pipeline.output, label)
             command = command_line + [f"--general-output {outdir} --general-msdir {msdir}"]
             command = " ".join(command)
-            LOG.info(f"Launching job using slurm. SPW={band} \n{self.slurm.__str__()}")
+            log.info(f"Launching job using slurm. SPW={band} \n{self.slurm.__str__()}")
             runstring = f"{command} {self.var} '{band}'"
             job = self.slurm.sbatch(runstring)
-            LOG.info(f"Job {job} is running: {runstring} ")
+            log.info(f"Job {job} is running: {runstring} ")
             self.jobs.append(job)
