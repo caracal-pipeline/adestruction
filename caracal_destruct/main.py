4bb7062af4481ea0d3c7a38e9d0a69d244a85572
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2026-03-03 18:30:03 +0200

    formatting

--- caracal_destruct/main.py
+++ caracal_destruct/main.py
@@ -50,9 +50,9 @@ from caracal_destruct.utils import File
     "--version",
     is_flag=True,
     help="Show the version and exit.",
-    callback=lambda ctx, param, value: click.echo(f"adestruction version {__version__}") or ctx.exit()
-    if value
-    else None,
+    callback=lambda ctx, param, value: (
+        click.echo(f"adestruction version {__version__}") or ctx.exit() if value else None
+    ),
 )
 def driver(
     config_file, spw_split_spec, bands, batchconfig, skip, singularity_image_dir, boring, dryrun, log_level, version
